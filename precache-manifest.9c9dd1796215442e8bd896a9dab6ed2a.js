35368d0d5d8283b99f497bd19f5a22a1fe61d142
Jansen Samosa <jansen.samosa2@gmail.com>
2020-06-02 02:01:32 -0700

    Updates

---
+++ precache-manifest.9c9dd1796215442e8bd896a9dab6ed2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f2f3fe34b11521bc03558553221578b",
+    "url": "/pokemon-team-planner/index.html"
+  },
+  {
+    "revision": "9177469664f07e6135a4",
+    "url": "/pokemon-team-planner/static/css/main.d83e056b.chunk.css"
+  },
+  {
+    "revision": "a7dce5dba81288727c2f",
+    "url": "/pokemon-team-planner/static/js/2.9d085267.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokemon-team-planner/static/js/2.9d085267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9177469664f07e6135a4",
+    "url": "/pokemon-team-planner/static/js/main.3bbdb4ae.chunk.js"
+  },
+  {
+    "revision": "6a218a7d4539e855dbd8",
+    "url": "/pokemon-team-planner/static/js/runtime-main.39306a3b.js"
+  }
+]);
