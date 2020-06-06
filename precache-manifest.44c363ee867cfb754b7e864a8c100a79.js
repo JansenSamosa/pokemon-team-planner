d5f95f042d446dfed78b781db47a6b92774fbfc6
Jansen Samosa <jansen.samosa2@gmail.com>
2020-06-05 23:11:38 -0700

    Updates

--- precache-manifest.44c363ee867cfb754b7e864a8c100a79.js
+++ precache-manifest.44c363ee867cfb754b7e864a8c100a79.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "651b16cce92632d8b43800631b797945",
+    "revision": "bf99b96f65cb42ad10e22bae893b2ed5",
     "url": "/pokemon-team-planner/index.html"
   },
   {
-    "revision": "d2a240ed9490e41c924b",
+    "revision": "dcd30353bca835acd9a3",
     "url": "/pokemon-team-planner/static/css/main.79e0b3f1.chunk.css"
   },
   {
-    "revision": "07d1e9bab81364be547e",
-    "url": "/pokemon-team-planner/static/js/2.6a7d2d9c.chunk.js"
+    "revision": "87d34a5a3eec0fda0c0a",
+    "url": "/pokemon-team-planner/static/js/2.61b986cc.chunk.js"
   },
   {
     "revision": "02de9bd29bbabca8706c9d7d000d9130",
-    "url": "/pokemon-team-planner/static/js/2.6a7d2d9c.chunk.js.LICENSE.txt"
+    "url": "/pokemon-team-planner/static/js/2.61b986cc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d2a240ed9490e41c924b",
-    "url": "/pokemon-team-planner/static/js/main.c1b0d99e.chunk.js"
+    "revision": "dcd30353bca835acd9a3",
+    "url": "/pokemon-team-planner/static/js/main.0034076f.chunk.js"
   },
   {
     "revision": "6a218a7d4539e855dbd8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pokemon-team-planner/precache-manifest.b9d3a8c51a3de45a722e25456be650a1.js"
+  "/pokemon-team-planner/precache-manifest.44c363ee867cfb754b7e864a8c100a79.js"
 );
 
 self.addEventListener('message', (event) => {
